dee0403070672e1f00e2aa12f0cacf4bc3bd1526
peng-ju <ju26@purdue.edu>
2023-11-20 17:12:10 -0500

    update constant

--- physics_constant.py
+++ physics_constant.py
@@ -11,14 +11,17 @@ class Physics_Constant():
         self.h = 6.62607e-34  # Planck constant [J/s]
         self.hbar = 1.05457e-34  # reduced Planck constant [J/s]
         self.G = 6.67430e-11  # Gravity constant [N*m^2*kg^{-2}]
+        self.g = 9.81  # The acceleration of gravity [m/s^2]
         self.mu_B =  9.27401e-24  # Bohr magneton [J/T]
         self.e = 1.60217e-19 # elementary charge [C]
-
+        self.c = 299792458  # the speed of light in vacuum [m/s]
 
         # density of materials
         self.rho_silica = 2e3  # density of silica [kg/m^3]
         self.rho_diamond = 3.5e3  # density of diamond [kg/m^3]
-
+        
+        # refractive index
+        # self.silica = 
 
 if __name__ == "__main__":
     constant = Physics_Constant()
